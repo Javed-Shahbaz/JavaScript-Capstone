4148d338ed3677ffe1b6ecebc73fc3fbd404a77c
Larry Villegas <larry_villegas@hotmail.com>
2023-04-26 10:42:31 -0400

    Create App.js

---
+++ src/modules/App.js
@@ -0,0 +1,58 @@
+import  getData  from './ApiSimpson.js';
+import iconLike from '../assests/like.png';
+
+// Render cards
+async function renderCards() {
+  // Data from simpson api
+  const data = await getData();
+  const CardsContainer = document.getElementById('dinamic-content');
+
+  data.forEach((episode) => {
+    const doc = document;
+    const article = doc.createElement('article');
+    article.classList.add('card');
+    const image = doc.createElement('img');
+    image.classList.add('thumbnail-image');
+    image.src = episode.thumbnailUrl;
+    image.alt= 'thumbnail image';
+    const divName = doc.createElement('div');
+    divName.classList.add('cont-name-like');
+    const pName = doc.createElement('p');
+    pName.classList.add('episode');
+    pName.textContent=episode.name;
+    const divLike = doc.createElement('div');
+    divLike.classList.add('cont-like');
+    const imgLike = doc.createElement('img');
+    imgLike.src= iconLike;
+    imgLike.classList.add('like');
+    imgLike.alt = 'like button';
+    const pNroLike = doc.createElement('p');
+    pNroLike.classList.add('n-likes');
+    const spanNro = doc.createElement('span');
+    spanNro.classList.add('nro-like');
+    spanNro.textContent = '5 ';
+    console.log("🚀 ~ file: App.js:34 ~ data.forEach ~ spanNro:", spanNro)
+    pNroLike.textContent ='likes #';
+    pNroLike.appendChild(spanNro);
+    console.log("🚀 ~ file: App.js:35 ~ data.forEach ~ pNroLike:", pNroLike)
+    const btnCmts = doc.createElement('button');
+    btnCmts.classList.add('comments');
+    btnCmts.textContent='Comments';
+    divLike.appendChild(imgLike);
+    divLike.appendChild(pNroLike);
+    divName.appendChild(pName);
+    divName.appendChild(divLike);
+    article.appendChild(image);
+    article.appendChild(divName);
+    article.appendChild(btnCmts);
+    
+    CardsContainer.appendChild(article);
+    
+  });
+}
+
+const App = () => {
+  renderCards();
+}
+
+export default App;
