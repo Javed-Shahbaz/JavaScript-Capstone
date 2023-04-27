9791f0b5316c3e74539d45dd35a97c7c480a638c
Larry Villegas <larry_villegas@hotmail.com>
2023-04-27 01:46:53 -0400

    Update ApiSimpson.js

--- src/modules/ApiSimpson.js
+++ src/modules/ApiSimpson.js
@@ -3,7 +3,8 @@ import { API_URL1 } from './Vars.js'; // , API_URL2
 async function getData() {
   const resp = await fetch(API_URL1);
   const json = await resp.json();
-  return json.slice(0, 6);
+  // return json.slice(0, 6);
+  return json;
 }
 
 export default getData;
