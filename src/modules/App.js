2b9906538faf780ac1dee00ba576c2ab2288e81f
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-04-29 10:16:58 +0500

    update app.js

--- src/modules/App.js
+++ src/modules/App.js
@@ -1,16 +1,19 @@
-import { getData, countOccurrences } from './ApiSimpson.js';
-import iconLike from '../assests/like.png';
-import getLikes from './GetLikes.js';
-import sendLike from './SendLike.js';
+import { getData, countOccurrences } from "./ApiSimpson.js";
+import iconLike from "../assests/like.png";
+import getLikes from "./GetLikes.js";
+import sendLike from "./SendLike.js";
+import { addComment } from "./addComments.js";
 // import getComments from './GetComments.js';
 
 // Render cards
 async function renderCards() {
   // Data from simpson api
   const data = await getData();
-  const CardsContainer = document.getElementById('dinamic-content');
+  const CardsContainer = document.getElementById("dinamic-content");
 
-  const nLikesArray = await Promise.all(data.map((episode) => getLikes(episode.id)));
+  const nLikesArray = await Promise.all(
+    data.map((episode) => getLikes(episode.id))
+  );
   data.forEach((episode, index) => {
     const nLikes = nLikesArray[index];
 
@@ -18,33 +21,33 @@ async function renderCards() {
     // for (const episode of data) {
     // const nLikes = await getLikes(episode.id);
     const doc = document;
-    const article = doc.createElement('article');
-    article.classList.add('card');
-    const image = doc.createElement('img');
-    image.classList.add('thumbnail-image');
+    const article = doc.createElement("article");
+    article.classList.add("card");
+    const image = doc.createElement("img");
+    image.classList.add("thumbnail-image");
     image.src = episode.thumbnailUrl;
-    image.alt = 'thumbnail image';
-    const divName = doc.createElement('div');
-    divName.classList.add('cont-name-like');
-    const pName = doc.createElement('p');
-    pName.classList.add('episode');
+    image.alt = "thumbnail image";
+    const divName = doc.createElement("div");
+    divName.classList.add("cont-name-like");
+    const pName = doc.createElement("p");
+    pName.classList.add("episode");
     pName.textContent = episode.name;
-    const divLike = doc.createElement('div');
-    divLike.classList.add('cont-like');
-    const imgLike = doc.createElement('img');
+    const divLike = doc.createElement("div");
+    divLike.classList.add("cont-like");
+    const imgLike = doc.createElement("img");
     imgLike.src = iconLike;
-    imgLike.classList.add('like');
-    imgLike.alt = 'like button';
-    const pNroLike = doc.createElement('p');
-    pNroLike.classList.add('n-likes');
-    const spanNro = doc.createElement('span');
-    spanNro.classList.add('nro-like');
+    imgLike.classList.add("like");
+    imgLike.alt = "like button";
+    const pNroLike = doc.createElement("p");
+    pNroLike.classList.add("n-likes");
+    const spanNro = doc.createElement("span");
+    spanNro.classList.add("nro-like");
     spanNro.textContent = nLikes;
-    pNroLike.textContent = 'likes #';
+    pNroLike.textContent = "likes #";
     pNroLike.appendChild(spanNro);
-    const btnCmts = doc.createElement('button');
-    btnCmts.classList.add('comments');
-    btnCmts.textContent = 'Comments';
+    const btnCmts = doc.createElement("button");
+    btnCmts.classList.add("comments");
+    btnCmts.textContent = "Comments";
     divLike.appendChild(imgLike);
     divLike.appendChild(pNroLike);
     divName.appendChild(pName);
@@ -55,59 +58,55 @@ async function renderCards() {
 
     CardsContainer.appendChild(article);
     // Detect the clik on like
-    imgLike.addEventListener('click', async () => {
+    imgLike.addEventListener("click", async () => {
       const itemId = episode.id;
       const nLikes = await getLikes(itemId);
       const output = await sendLike(itemId);
       if (output) {
         spanNro.textContent = nLikes + 1;
       } else {
-        spanNro.textContent = 'error';
+        spanNro.textContent = "error";
       }
     });
-
-    const popup = doc.getElementById('popup');
-    btnCmts.addEventListener('click', async () => {
-      popup.classList.add('show');
-      console.log(episode);
-      document.getElementById('img-popup').src = episode.thumbnailUrl;
-      document.getElementById('popup-name').textContent = episode.name;
-      document.getElementById('ep-number').textContent = episode.episode;
-      document.getElementById('desc').textContent = episode.description;
-      const frmAddComment = document.querySelector('.addForm');
-      frmAddComment.addEventListener('submit', (event) => {
-        console.log('clecked  ads fdsaf asdf sdaf asd');
-        event.preventDefault();
-        const name = frmAddComment.name.value;
-        const comment = frmAddComment.inputcomments.value;
-        console.log(comment);
-        const itemId = episode.id;
-
-        const d = new Date();
-        const commentDescription = document.getElementById('comments');
-        // const commentConter = document.querySelector('#totalComment');
-
-        const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
-        const mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(d);
-        const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
-        commentDescription.innerHTML += `<li>${ye}-${mo}-${da}  ${name}:   ${comment}</li>`;
-        // commentConter.innerHTML = Number(commentConter.innerHTML) + 1;
-        frmAddComment.reset();
-
-        showComment(itemId, name, comment);
-      })
-    });
-
-    const cbtn = doc.getElementById('cross-btn');
-    cbtn.addEventListener('click', () => {
-      popup.classList.remove('show');
+    const popup = doc.getElementById("popup");
+    btnCmts.addEventListener("click", async () => {
+      popup.classList.add("show");
+      document.getElementById("img-popup").src = episode.thumbnailUrl;
+      document.querySelector(".popup-name").textContent = episode.name;
+      document.getElementById("ep-number").textContent = episode.episode;
+      document.getElementById("desc").textContent = episode.description;
+      document.querySelector(".addForm").setAttribute("id", `${episode.id}`);
     });
   });
 }
 
+const frmAddComment = document.getElementById("addCommentBtn");
+frmAddComment.addEventListener("click", (event) => {
+  event.preventDefault();
+  const name = document.querySelector(".inputname").value;
+  const comment = document.querySelector("#inputcomments").value;
+  const itemId = document.querySelector("#addCommentBtn").parentElement.id;
+  if (name !== "" && comment !== "") {
+    const commentObj = JSON.stringify({
+      "item_id": itemId,
+      "username": name,
+      "comment": comment,
+    });
+
+    addComment(commentObj);
+    document.querySelector(".inputname").value = "";
+    document.querySelector("#inputcomments").value = "";
+  }
+});
+
+const cbtn = document.getElementById("cross-btn");
+cbtn.addEventListener("click", () => {
+  popup.classList.remove("show");
+});
+
 const displayCountItems = async () => {
   const nro = await countOccurrences();
-  document.getElementById('nro').innerHTML = nro;
+  document.getElementById("nro").innerHTML = nro;
 };
 
 const App = () => {
